fa83c49776b9bf441c196e043cea4f3de15e4864
PilotJaguar <marimbe2002@gmail.com>
2024-10-29 09:11:39 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function pageTransitions() {
+    // Button click active class
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function () {
+            let currentBtn = document.querySelector('.active-btn');
+            if (currentBtn) {
+                currentBtn.classList.remove('active-btn');
+            }
+            this.classList.add('active-btn');
+        });
+    }
+
+    // Sections active 
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            // Remove selected from other buttons
+            sectBtn.forEach((btn) => {
+                btn.classList.remove('active');
+            });
+            e.target.classList.add('active');
+
+            // Hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            });
+
+            // Show the target section
+            const element = document.getElementById(id);
+            if (element) {
+                element.classList.add('active');
+            }
+        }
+    });
+}
+
+pageTransitions();
